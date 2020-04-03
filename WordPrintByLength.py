64023e8a844ac6808e852ba8ce43d5b2719904dd
insertgamedevnamehere <62914153+insertgamedevnamehere@users.noreply.github.com>
2020-04-02 22:18:11 -0700

    Create WordPrintByLength.py

---
+++ WordPrintByLength.py
@@ -0,0 +1,5 @@
+word = input("Please type a word: ")	# get a word via user input
+word_length = len(word)					# store the length of string
+
+for letter in word:						# print the word based on how many letters are in it
+	print(word)
